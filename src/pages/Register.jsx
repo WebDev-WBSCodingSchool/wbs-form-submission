d2857a3c94c902ea68ea1d0269222f8172f58726
Jorge Paul <jserranopaul@gmail.com>
2025-05-28 08:50:35 +0200

    Register

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,16 +1,34 @@
 import { ErrorBoundary } from 'react-error-boundary';
+import { useFormStatus } from 'react-dom';
+import { toast } from 'react-toastify';
 import { ErrorFallback } from '../components';
+import { registerNewsletter } from '../api';
+
+const Submit = () => {
+  const { pending } = useFormStatus();
+  return (
+    <button className='btn btn-neutral mt-4' disabled={pending}>
+      {pending ? 'Registering...' : 'Register'}
+    </button>
+  );
+};
 
 const Register = () => {
+  const action = async formData => {
+    const email = formData.get('email');
+    await registerNewsletter(email);
+    toast.success(`Successfully register ${email}`);
+  };
+
   return (
     <div className='flex flex-col items-center'>
       <ErrorBoundary FallbackComponent={ErrorFallback}>
-        <form>
+        <form action={action}>
           <fieldset className='fieldset bg-base-200 border-base-300 rounded-box w-lg border p-4'>
             <legend className='fieldset-legend'>Register to our newsletter</legend>
             <label className='label'>Email</label>
             <input className='input w-full' name='email' placeholder='Email' />
-            <button className='btn btn-neutral mt-4'>Register!</button>
+            <Submit />
           </fieldset>
         </form>
       </ErrorBoundary>
